b230e0fe76f36145abef030d1bb3633328f4e794
battleangel217 <idaraobong05@gmail.com>
2025-10-26 18:20:14 +0100

    fix db issue

--- HNG3/settings.py
+++ HNG3/settings.py
@@ -82,16 +82,18 @@ WSGI_APPLICATION = 'HNG3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'hng3_countries'),
-        'USER': os.getenv('DB_USER', 'root'),
+        'NAME': os.getenv('DB_NAME', 'railway'),
+        'USER': os.getenv('DB_USER', 'railway'),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
+
 
 
 # Password validation
